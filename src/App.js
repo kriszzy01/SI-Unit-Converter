bd7524338775016685ce93e13412617984b30d00
Christian Otu <christianotu.d@gmail.com>
2020-07-30 01:59:39 +0100

    Made changes to Home Component on App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import { AreaConverter } from "./features/units/area/AreaConverter";
-import { LengthConverter } from "./features/units/length/LengthConverter";
-import { VolumeConverter } from "./features/units/volume/VolumeConverter";
-import { DataConverter } from "./features/units/data/DataConverter";
-import { MassConverter } from "./features/units/mass/MassConverter";
-import { ConverterHeader } from "./features/ConverterHeader";
+import {LengthConverter} from "./features/units/length/LengthConverter";
+import {VolumeConverter} from "./features/units/volume/VolumeConverter";
+import {DataConverter} from "./features/units/data/DataConverter";
+import {MassConverter} from "./features/units/mass/MassConverter";
+import {ConverterHeader} from "./features/ConverterHeader";
 
 export const App = () => {
 
     return (
         <Router>
-            <ConverterHeader />
+            <ConverterHeader/>
             <Switch>
-                <Route
-                    exact path="/"
-                    render={() => AreaConverter}
-                />
-                <Route path="/length"><LengthConverter /></Route>
-                <Route path="/volume"><VolumeConverter /></Route>
-                <Route path="/data"><DataConverter /></Route>
-                <Route path="/mass"><MassConverter /></Route>
+                <Route exact path="/"><AreaConverter/></Route>
+                <Route path="/length"><LengthConverter/></Route>
+                <Route path="/volume"><VolumeConverter/></Route>
+                <Route path="/data"><DataConverter/></Route>
+                <Route path="/mass"><MassConverter/></Route>
             </Switch>
         </Router>
     );
